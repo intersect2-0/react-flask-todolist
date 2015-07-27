d0742d520cf50931a1e6ff640e4b7d5e14e09c86
Ivan <intersect2-0@users.noreply.github.com>
2015-07-27 21:56:21 +1000

    Added Migrations and table for users

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
-from server import app
+from server import app, db
+app.config.from_object('config')
 
+migrate = Migrate(app, db)
 manager = Manager(app)
 
+manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
     manager.run()
---
+++ models.py
@@ -0,0 +1,27 @@
+from server import db
+import datetime
+
+
+class SiteUsers(db.Model):
+    __tablename__ = 'siteusers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(30), unique=True)
+    password = db.column(db.String(20))
+    registered_on = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<User: {}'.format(self.caption)
