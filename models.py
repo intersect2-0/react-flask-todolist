0da32e2376fb11387b793c67f43bbcff626de69f
Ivan <intersect2-0@users.noreply.github.com>
2015-07-29 18:10:50 +1000

    small fix

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class SiteUsers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(30), unique=True)
-    password = db.column(db.String(30))
+    password = db.Column(db.String(30))
     registered_on = db.Column(db.DateTime, default=datetime.datetime.now())
 
     def is_authenticated(self):
