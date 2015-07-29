c7fe7958e57bc14d85643a0b44e97b1bf7e6524c
Ivan <intersect2-0@users.noreply.github.com>
2015-07-29 21:19:57 +1000

    Added Todo Model

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class SiteUsers(db.Model):
     email = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(30))
     registered_on = db.Column(db.DateTime, default=datetime.datetime.now())
+    todos = db.relationship('Todo', backref='user', lazy='dynamic')
 
     def is_authenticated(self):
         return True
@@ -25,3 +26,14 @@ class SiteUsers(db.Model):
 
     def __repr__(self):
         return '<User: {}'.format(self.username)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.UnicodeText)
+    created_on = db.Column(db.Date, default=datetime.datetime.now().date())
+    done = db.Column(db.Boolean, default=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('siteusers.id'))
